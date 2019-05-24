1d5857faaab9da7c589828c7000acf066718e053
Liam <l.swift94@gmail.com>
2019-05-24 14:40:18 +1000

    mongodb connect successfully

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db')
+
+//Connect DB
+
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
